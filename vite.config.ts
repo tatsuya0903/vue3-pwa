b3362a2a00c21a893205d2023189def847300138
tatsuya0903 <tama.and.pochi.0903@gmail.com>
2022-03-26 00:25:55 +0900

    vue add vuetify
    📦  Installing vue-cli-plugin-vuetify...
    
    added 11 packages, and audited 486 packages in 3s
    
    77 packages are looking for funding
      run `npm fund` for details
    
    found 0 vulnerabilities
    ✔  Successfully installed plugin: vue-cli-plugin-vuetify
    
    ? Choose a preset: Vite Preview (Vuetify 3 + Vite)
    
    🚀  Invoking generator for vue-cli-plugin-vuetify...
    📦  Installing additional dependencies...
    
    added 26 packages, and audited 512 packages in 11s
    
    83 packages are looking for funding
      run `npm fund` for details
    
    found 0 vulnerabilities
    ⚓  Running completion hooks...
    
    ✔  Successfully invoked generator for plugin: vue-cli-plugin-vuetify
     vuetify  Discord community: https://community.vuetifyjs.com
     vuetify  Github: https://github.com/vuetifyjs/vuetify
     vuetify  Support Vuetify: https://github.com/sponsors/johnleider

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,36 @@
-import { fileURLToPath, URL } from 'url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify from '@vuetify/vite-plugin'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue3-pwa/',
-  build: {
-    outDir: 'docs',
-  },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+    vuetify({
+      autoImport: true,
+    }),
+  ],
+  define: { 'process.env': {} },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, 'src'),
     },
   },
+  /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
+  resolve: {
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ]
+  },
+  */
 })
