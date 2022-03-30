ca34cf1998723e6c2c65e639567a306c9d8ae11c
tatsuya0903 <tama.and.pochi.0903@gmail.com>
2022-03-30 22:29:38 +0900

    Vite Plugin PWAを設定
    Edit your vite.config.ts file to add Vite Plugin PWA:
    https://vite-plugin-pwa.netlify.app/guide/#setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,7 +10,7 @@ export default defineConfig({
   build: {
     outDir: 'docs',
   },
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({})],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
