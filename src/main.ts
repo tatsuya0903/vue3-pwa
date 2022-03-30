d87b269c786f6c4928cfc31dc028b53b07c5939c
tatsuya0903 <tama.and.pochi.0903@gmail.com>
2022-03-30 23:58:04 +0900

    registerSWを実行
    Prompt for new content refreshing | Guide | Vite Plugin PWA
    https://vite-plugin-pwa.netlify.app/guide/prompt-for-update.html#runtime

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { registerSW } from 'virtual:pwa-register'
 
+const updateSW = registerSW({
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onNeedRefresh() {},
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onOfflineReady() {},
+})
 const app = createApp(App)
 
 app.use(createPinia())
